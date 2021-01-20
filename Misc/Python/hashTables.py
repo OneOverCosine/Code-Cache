0d202fb730b89d18435927bc094cc4e1201e8d40
OneOverCosine <TheCandyDragon@gmail.com>
2021-01-20 10:36:49 +0000

    Create hashTables.py

---
+++ Misc/Python/hashTables.py
@@ -0,0 +1,9 @@
+# to get more comfortable with hashtables in python
+
+ratingList = {'Sleepy' : 7, 'Cringe Katalyst' : 5, 'Langeweile' : 9, 'Baroness Boredom' : 2, 'SecantTheta' : 6}
+
+print(ratingList)
+
+name = 'Langeweile'
+
+print('The best one so far is ' + name + ' with a score of ' + str(ratingList[name]))
