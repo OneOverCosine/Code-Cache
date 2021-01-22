06eaf7e58a3c9bf5c70c79945328c0d068961df5
OneOverCosine <TheCandyDragon@gmail.com>
2021-01-22 09:36:36 +0000

    made changes to code
    Added:
    - twoStrings.py
    - regularExpressions.py

---
+++ HackerRank/Dictionaries_and_Hashmaps/Python/twoStrings.py
@@ -0,0 +1,25 @@
+''' to sumarise, I want to find out if s1 and s2 share
+a common substring. That means as soon as I find 1 (even
+if there are multiple) I can return YES '''
+
+import re
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    ''' things to keep in mind...
+    strings can be of length 10^5 (100000)
+    '''
+
+    # for the sake of testing
+    print('twoStrings() is running')
+
+    # may not need this
+    s1Arr = s1.split()
+    s2Arr = s2.split()
+
+def main():
+
+    print('main() is running')
+
+    s1 = 'art'
+    s2 = 'and'
---
+++ Misc/Python/regularExpressions.py
@@ -0,0 +1,16 @@
+""" getting more comfortable using regex
+read about it here: https://www.w3schools.com/python/python_regex.asp
+"""
+
+import re
+
+# https://stackoverflow.com/a/9477447/11304289 may use for Two Strings problem
+
+s1 = "I don't even have time to explain why I don't have time to explain"
+sComp1 = "therearenospacesherebecausetheyarenotrequired"
+sComp2 = "pressftopayrespects"
+
+sFor = re.findall('.?'*7, s1)
+
+for bit in sFor:
+    print(bit)
