606720186dd1efd7fc4c52c927b42bb26cf95377
OneOverCosine <TheCandyDragon@gmail.com>
2021-01-25 14:47:41 +0000

    Update twoStrings.py

--- HackerRank/Dictionaries_and_Hashmaps/Python/twoStrings.py
+++ HackerRank/Dictionaries_and_Hashmaps/Python/twoStrings.py
@@ -8,18 +8,33 @@ import re
 def twoStrings(s1, s2):
     ''' things to keep in mind...
     strings can be of length 10^5 (100000)
+    All I need to do is check that the two strings
+    contain one letter that's the same in both
     '''
 
-    # for the sake of testing
-    print('twoStrings() is running')
+    #create a dictionary for the first string
+    sDict = {}
 
-    # may not need this
-    s1Arr = s1.split()
-    s2Arr = s2.split()
+    # add each letter that occurs in s1 to dictionary
+    for char in s1:
+        if len(sDict) == 26:
+            return 'YES' # I should be able to return 'YES' here since s1 contains every letter of the alphabet at this point
+        elif char not in sDict.keys():
+            sDict[char] = 1
+
+    # now check if s2 contains the
+    for char in s2:
+        if char in sDict.keys():
+            return 'YES'
+
+    # if we get to this point, s1 and s2 have no substring in common
+    return 'NO'
 
 def main():
 
-    print('main() is running')
+    s1 = 'abcdefghijklmnopqrstuvw'
+    s2 = 'xzya'
 
-    s1 = 'art'
-    s2 = 'and'
+    print(twoStrings(s1, s2))
+
+main()
