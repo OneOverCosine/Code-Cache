f9986e20d2efe36628597d5ef6f931803c0b89c2
OneOverCosine <TheCandyDragon@gmail.com>
2021-02-12 15:35:53 +0000

    Create basics.py

---
+++ HackerRank/Introduction/Python/basics.py
@@ -0,0 +1,22 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if(year%400 == 0):
+        leap = True
+    elif(year%100 != 0 and year%4 == 0):
+        leap = True
+    
+    return leap
+
+
+def main():
+
+    print("Hi there")
+
+    year = 2020
+
+    print(is_leap(year))
+
+
+main()
